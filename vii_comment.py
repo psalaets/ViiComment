2a34d62ae0800efe2b5c009608666d4ff0f40278
Paul Salaets <psalaets@gmail.com>
2015-04-23 22:29:04 -0400

    clean up

--- vii_comment.py
+++ vii_comment.py
@@ -3,10 +3,18 @@ import sublime, sublime_plugin
 from datetime import date
 from string import Template
 
-# In ST2, param is a sublime.RegionSet. In ST3 it's a sublime.Selection
-# They appear to be the same API-wise
-def no_selected_text(selection):
-    for region in selection:
+class ViiComment(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.run_command('toggle_comment')
+        
+        if no_selected_text(self):
+            insert_snippet(self);
+
+def no_selected_text(self):
+    # In ST2 view.sel() is a sublime.RegionSet
+    # In ST3 it's a sublime.Selection
+    # They appear to be the same API-wise
+    for region in self.view.sel():
         if region.size() > 0:
             return False
     return True
@@ -14,17 +22,12 @@ def no_selected_text(selection):
 def read_initials():
     return sublime.load_settings('vii_comment.sublime-settings').get('initials')
 
-class ViiComment(sublime_plugin.TextCommand):
-    def run(self, edit):
-        self.view.run_command('toggle_comment')
-        
-        # insert snippet if there is no selected text
-        if no_selected_text(self.view.sel()):
-            today = date.today().strftime('%m/%d/%y')
-            initials = read_initials()
-            
-            snippet = Template('${initials} - $$1 ${date}').substitute(date=today, initials=initials)
-            
-            self.view.run_command('insert_snippet', {
-                'contents': snippet
-            })
+def insert_snippet(self):
+    today = date.today().strftime('%m/%d/%y')
+    initials = read_initials()
+    
+    snippet = Template('${initials} - $$1 ${date}').substitute(date=today, initials=initials)
+    
+    self.view.run_command('insert_snippet', {
+        'contents': snippet
+    })
