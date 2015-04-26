c90150c760d16cd6f1883765ae97f323af707647
vii <psalaets@viimed.com>
2015-04-26 15:45:53 -0400

    'no selected text' is covered by 'cursor lines are blank'

--- vii_comment.py
+++ vii_comment.py
@@ -13,9 +13,12 @@ class ViiComment(sublime_plugin.TextCommand):
             insert_snippet(self)
 
 def should_insert_snippet(self):
-    return no_selected_text(self) and cursor_lines_are_blank(self)
+    return cursor_lines_are_blank(self)
 
 def cursor_lines_are_blank(self):
+    # In ST2 view.sel() is a sublime.RegionSet
+    # In ST3 it's a sublime.Selection
+    # They appear to be the same API-wise
     for region in self.view.sel():
         cursor_line = self.view.line(region.begin())
         if not is_blank(self.view.substr(cursor_line)):
@@ -25,15 +28,6 @@ def cursor_lines_are_blank(self):
 # param: string
 def is_blank(line):
     return not line or line.isspace()
-
-def no_selected_text(self):
-    # In ST2 view.sel() is a sublime.RegionSet
-    # In ST3 it's a sublime.Selection
-    # They appear to be the same API-wise
-    for region in self.view.sel():
-        if region.size() > 0:
-            return False
-    return True
 
 def insert_snippet(self):
     today = date.today().strftime('%m/%d/%y')
